f934e446890dd57fcd1602ffb5a6dc84532c612a
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-12-02 12:40:14 +0330

    feat: add basic layout + page

---
+++ layouts/Landing.vue
@@ -0,0 +1,5 @@
+<template>
+  <LayoutsLandingHeader />
+  <slot></slot>
+  <LayoutsLandingFooter />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  layout: "landing",
+});
+</script>
 
 <template>
   <p>{{ $t("welcome") }}</p>
