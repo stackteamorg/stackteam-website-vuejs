395a4649a933190ccd9b3cf456aa96508d2533a8
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-12-02 12:39:27 +0330

    build: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{vue,ts}",
+    "./layouts/**/*.{vue,ts}",
+    "./components/**/*.{vue,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
