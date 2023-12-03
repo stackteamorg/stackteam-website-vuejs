908f54e98707b36c19cbac97b8c7e72fa5a5cdb1
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-12-03 17:07:39 +0330

    feat: open dropdown with js

--- components/layouts/landing/LanguageSelect.vue
+++ components/layouts/landing/LanguageSelect.vue
@@ -12,26 +12,33 @@ const languages = new Map([
 </script>
 
 <template>
-  <div class="group relative">
-    <Button severity="outlined">
+  <div
+    class="group relative"
+    @mouseenter="show = true"
+    @mouseleave="show = false">
+    <Button @click="show = true" severity="outlined">
       <div class="flex items-center gap-1">
         <ChevronDownIcon class="w-4 h-4" />
         <span class="text-sm">{{ languages.get($i18n.locale) }}</span>
       </div>
     </Button>
     <div
-      class="text-sm capitalize absolute top-full bg-white shadow rtl:right-0 ltr:left-0 hidden group-hover:block rounded-md min-w-[5rem] text-center overflow-hidden">
+      v-show="show"
+      class="text-sm absolute top-full bg-white shadow rtl:right-0 ltr:left-0 rounded-md min-w-full text-center overflow-hidden">
       <NuxtLink
+        @click="show = false"
         :to="switchLocalePath('en')"
         class="block w-full hover:bg-black/10 transition-colors duration-300 pt-1.5 pb-0.5">
         English
       </NuxtLink>
       <NuxtLink
+        @click="show = false"
         :to="switchLocalePath('fa')"
         class="block w-full hover:bg-black/10 transition-colors duration-300 py-0.5">
         فارسی
       </NuxtLink>
       <NuxtLink
+        @click="show = false"
         :to="switchLocalePath('ar')"
         class="block w-full hover:bg-black/10 transition-colors duration-300 pt-0.5 pb-1">
         العربية
