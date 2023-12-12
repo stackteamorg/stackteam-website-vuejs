7c5d67691f1be713c5aa59ae57a9316be78d8282
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-12-12 15:29:39 +0330

    feat: add floating cart to footer

--- src/components/layouts/landing/Footer.vue
+++ src/components/layouts/landing/Footer.vue
@@ -1,11 +1,75 @@
 <script setup lang="ts">
-const localePath = useLocalePath()
+// TODO - Translate
+import { ArrowLeftIcon, ArrowRightIcon, CheckIcon } from "@heroicons/vue/24/solid";
+
+const localePath = useLocalePath();
+const floatElementHeight = ref(0);
+const floatElement = ref<HTMLDivElement>();
+
+const sizeHandler = () => {
+  if (!floatElement.value) return;
+  floatElementHeight.value = floatElement.value.clientHeight;
+};
+
+onMounted(() => {
+  sizeHandler();
+
+  window.addEventListener("resize", sizeHandler);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", sizeHandler);
+});
 </script>
 
 <template>
+  <div class="mt-8" :style="{ height: `${floatElementHeight / 2}px` }"></div>
+
   <div
-    class="rounded-t-3xl bg-gradient-to-br from-dreamy-green-200 to-dreamy-green-300 pb-4 pt-16">
-    <div class="container-app container-padding mb-8 grid grid-cols-4 gap-5">
+    class="relative rounded-t-3xl bg-green-gradient pb-4 pt-16">
+    <div
+      ref="floatElement"
+      class="absolute bottom-full left-1/2 w-full max-w-screen-2xl -translate-x-1/2 translate-y-1/2 px-6">
+      <div
+        class="flex w-full flex-col items-center justify-between gap-6 rounded-2xl bg-secondary px-6 py-12 md:flex-row">
+        <img
+          :width="256"
+          :height="256"
+          class="hidden h-36 w-36 shrink-0 object-contain lg:block"
+          src="~/assets/images/subscraib_img.png"
+          alt="subscribe" />
+
+        <div class="grow space-y-4 text-white">
+          <p class="text-primary">UNLOCK THE POWER</p>
+          <p class="max-w-lg text-5xl font-bold">
+            Stop wasting time & money on content
+          </p>
+          <div class="flex flex-col items-baseline gap-4 md:flex-row">
+            <div class="flex items-center gap-2">
+              <CheckIcon class="text-primary-400 h-4 w-4" />
+              <p>No credit card required</p>
+            </div>
+            <div class="flex items-center gap-2">
+              <CheckIcon class="text-primary-400 h-4 w-4" />
+              <p>No credit card required</p>
+            </div>
+          </div>
+        </div>
+
+        <UButton
+          size="lg"
+          variant="whiteGhost"
+          type="submit"
+          class="flex shrink-0 items-center gap-2">
+          <p>{{ $t("collabration") }}</p>
+          <ArrowLeftIcon v-if="$i18n.locale !== 'en'" class="h-4 w-4" />
+          <ArrowRightIcon v-if="$i18n.locale === 'en'" class="h-4 w-4" />
+        </UButton>
+      </div>
+    </div>
+
+    <div :style="{ height: `${floatElementHeight / 2}px` }"></div>
+    <div class="container-app container-padding mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
       <div>
         <img
           src="@/assets/images/icon.png"
